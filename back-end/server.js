8d313c3b7b73fb9039e5731120178a9a4cc6fbec
r2c2ryan@gmail.com <r2c2ryan@gmail.com>
2016-11-09 22:36:37 -0800

    server js updates

--- back-end/server.js
+++ back-end/server.js
@@ -3,15 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-//var database;
-
-//uppercase M , something we want to instatiate and a model
 var Message = mongoose.model('Message', {
-    msg: String //define model
-})
+    msg: String
+});
 
 app.use(bodyParser.json());
-
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -23,27 +19,26 @@ app.get('/api/message', GetMessages)
 
 app.post('/api/message', function (req, res) {
     console.log(req.body);
-    //database.collection('messages').insertOne(req.body);
+
     var message = new Message(req.body);
+
     message.save();
+
     res.status(200);
 })
 
+function GetMessages(req, res) {
+    Message.find({}).exec(function (err, result) {
+        res.send(result);
+    })
+}
+
 mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
     if (!err) {
-        console.log("we are connected to mongo!!");
-     //   GetMessage(); //when connected - display all messages
-        //database = db;
+        console.log("we are connected to mongo");
     }
 })
 
 var server = app.listen(5000, function () {
     console.log('listening on port ', server.address().port)
 })
-
-function GetMessages(req, res) {
-    Message.find({}).exec(function (err, result) {
-        // console.log(result);
-        res.send(result);
-    })
-}
